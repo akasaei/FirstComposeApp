78be0998dcfda24e577ce6b8514377e78e813161
akasaei <a.kasaei@gmail.com>
2026-07-31 22:38:12 +0300

    remove stale method

--- app/src/main/java/com/ali/firstcomposeapp/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/ali/firstcomposeapp/viewmodel/OrderViewModel.kt
@@ -6,14 +6,12 @@ import com.ali.firstcomposeapp.data.repository.OrderRepository
 import com.ali.firstcomposeapp.model.Order
 import com.ali.firstcomposeapp.model.OrderUiState
 import com.ali.firstcomposeapp.viewmodel.event.OrderEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 @HiltViewModel
@@ -79,38 +77,6 @@ class OrderViewModel @Inject constructor(
         observeOrders()
         refreshOrders()
     }
-
-    private fun fetchOrders() {
-        viewModelScope.launch {
-            repository
-                .observeOrders()
-                .onStart {
-                    _uiState.update {
-                        it.copy(
-                            isLoading = true,
-                            error = null
-                        )
-                    }
-                }
-                .catch { exception ->
-                    _uiState.update {
-                        it.copy(
-                            isLoading = false,
-                            error = exception.message ?: "Unknown error"
-                        )
-                    }
-                }
-                .collect { orders ->
-                    _uiState.update {
-                        it.copy(
-                            isLoading = false,
-                            orders = orders
-                        )
-                    }
-                }
-        }
-    }
-
 
     private fun observeOrders() {
         viewModelScope.launch {
